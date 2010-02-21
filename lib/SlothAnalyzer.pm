e2b6eb562ae74f49dea227ee87e0ddaa79680d4e
hiratara <hiratara@cpan.org>
2010-02-21 14:16:24 +0900

    The first version.

--- lib/SlothAnalyzer.pm
+++ lib/SlothAnalyzer.pm
@@ -8,15 +8,48 @@ __END__
 
 =head1 NAME
 
-SlothAnalyzer -
+SlothAnalyzer - analyzes the git usages.
 
 =head1 SYNOPSIS
 
-  use SlothAnalyzer;
+Make sloth_contribution.yaml and run sloth_contribution.
+You'll get following output;
+
+  % sloth_contribution
+  SINCE: 1 month ago
+
+  [hiratara]
+  commits 37
+  files   465
+  inserts 2300
+  deletes 14537
+
+  [honma]
+  commits 11
+  files   19
+  inserts 103
+  deletes 65
+
+You can also specify the period for summary by the same format as the 
+'git log --since' format.
+
+  % sloth_contribution '1 week'
+  SINCE: 1 week
+  [hiratara]
+  commits 9
+  files   19
+  inserts 169
+  deletes 110
+
+  [honma]
+  commits 2
+  files   4
+  inserts 5
+  deletes 7
 
 =head1 DESCRIPTION
 
-SlothAnalyzer is
+SlothAnalyzer is for my own use. You may not use this module.
 
 =head1 AUTHOR
 
@@ -30,3 +63,4 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =cut
+
